81d175b33e48e3ce89f7176f03a387fa6a1d01ea
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2023-07-20 18:41:45 -0400

    basic UI components added to next project

---
+++ components/Main.jsx
@@ -0,0 +1,20 @@
+import { useWeb3Contract } from "react-moralis"
+
+export default function Main() {
+    // const {runContractFunction } = useWeb3Contract({
+    //     abi: //,
+    //     address: //,
+    //     functionName: //,
+    //     params: {},
+    //     msgValue: //,
+    // })
+
+    return (
+        <div className="test">
+            <h1>Ready Player Me</h1>
+            <p>
+                This is a demo of the Ready Player Me Avatar Creator and Viewer.
+            </p>
+        </div>
+    )
+}
